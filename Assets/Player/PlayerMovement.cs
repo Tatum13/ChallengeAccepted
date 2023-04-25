652733ea24fd4af6f199d14a13fdd42726b281d6
Tatum13 <tatumdv2001@gmail.com>
2023-04-25 20:55:07 +0200

    applied feedback

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,17 +6,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float rotateSpeed;
     
-    private Vector3 _direction;
-
     public void MovementPlayer(Vector2 input)
     {
+        if (input == Vector2.zero) return;
+        
         var input3D = new Vector3(input.x, rigidbody.velocity.y / speed, input.y);
         rigidbody.velocity = input3D * speed;
-        if (input != Vector2.zero)
-        { 
-            var rotateTarget = new Vector3(input.x, 0, input.y);
-            var rotateDirection = Vector3.RotateTowards(transform.forward, rotateTarget, rotateSpeed * Time.deltaTime, 1);
-            transform.rotation = Quaternion.LookRotation(rotateDirection); 
-        }
+        
+        var rotateTarget = new Vector3(input.x, 0, input.y);
+        var rotateDirection = Vector3.RotateTowards(transform.forward, rotateTarget, rotateSpeed * Time.deltaTime, 1);
+        transform.rotation = Quaternion.LookRotation(rotateDirection);
     }
 }
