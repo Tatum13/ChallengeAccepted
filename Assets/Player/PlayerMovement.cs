66dbbc8a23e9bbb938aaba6bd907ca0d5737da0b
Tatum13 <tatumdv2001@gmail.com>
2023-04-25 20:44:55 +0200

    added rotation with the movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
     [SerializeField] private float speed;
+    [SerializeField] private float rotateSpeed;
     
     private Vector3 _direction;
 
@@ -14,5 +15,11 @@ public class PlayerMovement : MonoBehaviour
     {
         var input3D = new Vector3(input.x, rigidbody.velocity.y / speed, input.y);
         rigidbody.velocity = input3D * speed;
+        if (input != Vector2.zero)
+        { 
+            var rotateTarget = new Vector3(input.x, 0, input.y);
+            var rotateDirection = Vector3.RotateTowards(transform.forward, rotateTarget, rotateSpeed * Time.deltaTime, 1);
+            transform.rotation = Quaternion.LookRotation(rotateDirection); 
+        }
     }
 }
