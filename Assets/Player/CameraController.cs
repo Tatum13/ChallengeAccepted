18136c38f5fdd7c29d397f13651f85817af22472
Tatum13 <tatumdv2001@gmail.com>
2023-05-03 23:16:29 +0200

    applied feedback

--- Assets/Player/CameraController.cs
+++ Assets/Player/CameraController.cs
@@ -1,24 +1,16 @@
-using UnityEngine.InputSystem;
 using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineFreeLook cinemachineFreeLook;
-    [SerializeField] private float cameraXSpeed;
-    [SerializeField] private float cameraYSpeed;
+    [SerializeField] private Vector2 cameraMaxSpeed;
 
     public void MoveCamera(float inputCamera)
     {
-        if (1 == Mathf.RoundToInt(inputCamera))
-        {
-            cinemachineFreeLook.m_XAxis.m_MaxSpeed = cameraXSpeed;
-            cinemachineFreeLook.m_YAxis.m_MaxSpeed = cameraYSpeed;
-        }
-        else
-        {
-            cinemachineFreeLook.m_XAxis.m_MaxSpeed = 0;
-            cinemachineFreeLook.m_YAxis.m_MaxSpeed = 0;
-        }
+        var mouseButtonIsPressed = 1 == Mathf.RoundToInt(inputCamera);
+        
+        cinemachineFreeLook.m_XAxis.m_MaxSpeed = mouseButtonIsPressed ? cameraMaxSpeed.x : 0;
+        cinemachineFreeLook.m_YAxis.m_MaxSpeed = mouseButtonIsPressed ? cameraMaxSpeed.y : 0;
     }
 }
